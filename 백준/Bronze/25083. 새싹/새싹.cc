c213c0b9663772114491dbd85c4e65587aea2319
KangHojun <49914295+gangside@users.noreply.github.com>
2022-08-04 20:12:18 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -1,12 +1,12 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
-    cout << "         ,r'\"7" << "\n";
-    cout << "r`-_   ,'  ,/" << "\n";
-    cout << " \\. \". L_r'" << "\n";
-    cout << "   `~\\/" << "\n";
-    cout << "      |" << "\n";
-    cout << "      |";
+    printf("         ,r\'\"7\n");
+    printf("r`-_   ,\'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |");
 }
