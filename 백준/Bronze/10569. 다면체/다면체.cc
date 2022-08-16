069e3e08ea36e886f27911806528dce2fbe581cc
KangHojun <49914295+gangside@users.noreply.github.com>
2022-08-16 09:04:48 +0900

    [Bronze III] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << 2 - a + b << "\n";
+    }
+}
